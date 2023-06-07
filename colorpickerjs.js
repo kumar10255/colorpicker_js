fe1314beec73e314f23939fd18569429bb4753ba
kumar <kkumar10255@gmail.com>
2023-06-07 12:20:04 +0530

    origin

---
+++ colorpickerjs.js
@@ -0,0 +1,19 @@
+function graycolor() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#e0e0e0";
+    document.getElementById("selectedColorHexCode").textContent = "#e0e0e0";
+}
+
+function greencolor() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#6fcf97";
+    document.getElementById("selectedColorHexCode").textContent = "#6fcf97";
+}
+
+function bluecolor() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#56ccf2";
+    document.getElementById("selectedColorHexCode").textContent = "#56ccf2";
+}
+
+function pinkcolor() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#bb6bd9";
+    document.getElementById("selectedColorHexCode").textContent = "#bb6bd9";
+}
